dad30b56982f63b807f8011ec339da95dbcae1bd
biinggala <49203064+biinggala@users.noreply.github.com>
2021-07-26 17:28:45 +0900

    setup - install babel and prima
    installed babel for using modern codes and prisma for making database

---
+++ server.js
@@ -0,0 +1,51 @@
+import { ApolloServer, gql } from 'apollo-server';
+import {
+    ApolloServerPluginLandingPageGraphQLPlayground
+    } from "apollo-server-core";
+
+// The GraphQL schema
+const typeDefs = gql`
+type Movie {
+    title: String
+    year: Int
+
+}
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+      createMovie(title: String!): Boolean
+      deleteMovie(title: String!): Boolean
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 })
+  },
+  Mutation: {
+      createMovie: (_, {title}) => {
+          console.log(title);
+          return true;
+        },
+      deleteMovie: (_, {title}) => {
+           console.log(title);
+           return true;
+      }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
