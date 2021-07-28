71aebc482d8c5def8f273381e17d154f1ce82d81
biinggala <49203064+biinggala@users.noreply.github.com>
2021-07-28 16:55:18 +0900

    end of practice with apollo and prisma.
    example of movie data

--- server.js
+++ server.js
@@ -1,51 +1,15 @@
-import { ApolloServer, gql } from 'apollo-server';
-import {
-    ApolloServerPluginLandingPageGraphQLPlayground
-    } from "apollo-server-core";
-
-// The GraphQL schema
-const typeDefs = gql`
-type Movie {
-    title: String
-    year: Int
-
-}
-  type Query {
-    movies: [Movie]
-    movie: Movie
-  }
-  type Mutation {
-      createMovie(title: String!): Boolean
-      deleteMovie(title: String!): Boolean
-  }
-`;
-
-// A map of functions which return data for the schema.
-const resolvers = {
-  Query: {
-    movies: () => [],
-    movie: () => ({ title: "Hello", year: 2021 })
-  },
-  Mutation: {
-      createMovie: (_, {title}) => {
-          console.log(title);
-          return true;
-        },
-      deleteMovie: (_, {title}) => {
-           console.log(title);
-           return true;
-      }
-  }
-};
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground(),
-    ],
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+const PORT = process.env.PORT;
+
+server.listen(PORT).then(() => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
